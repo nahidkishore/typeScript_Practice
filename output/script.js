fa1ba4e2a1fdea39bff66b26e635a6bfe664fc14
Nahidul Islam <nahidkishore99.com>
2021-12-27 14:27:07 +0600

    typescript practice

---
+++ output/script.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('hello');
+const country = ' I Love Bangladesh';
+let myName = 'nahid';
+let fruits = ['apple', 'orange', 'banana', '33'];
+fruits.push(22);
+console.log(country);
+console.log(myName);
---
+++ src/script.ts
@@ -0,0 +1,9 @@
+console.log('hello')
+const country = ' I Love Bangladesh'
+let myName = 'nahid'
+let fruits = ['apple', 'orange', 'banana', '33']
+fruits.push(22)
+
+
+console.log(country)
+console.log(myName)
